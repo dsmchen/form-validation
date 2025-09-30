02776a22d1e65c20ba00a1d55f8dd43cbb1d4982
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2025-09-30 16:22:07 +0800

    Add email and postal code validation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
 import "./style.css";
+
+// Validate email
+
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+email.addEventListener("input", () => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+  } else {
+    showEmailError();
+  }
+});
+
+function showEmailError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "Email is required";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Invalid email format";
+  }
+}
+
+// Validate postal code
+
+const postalCode = document.getElementById("postal-code");
+const country = document.getElementById("country");
+const postalCodeError = document.querySelector("#postal-code + span.error");
+
+function checkPostalCode() {
+  const constraints = {
+    ch: [
+      "^(CH-)?\\d{4}$",
+      "Swiss postal codes must have exactly 4 digits: e.g. CH-1234 or 1234",
+    ],
+    fr: [
+      "^(F-)?\\d{5}$",
+      "French postal codes must have exactly 5 digits: e.g. F-12345 or 12345",
+    ],
+    de: [
+      "^(D-)?\\d{5}$",
+      "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+    ],
+    nl: [
+      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+      "Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS: e.g. 1234 AB",
+    ],
+  };
+
+  const countryValue = country.value;
+  const constraint = new RegExp(constraints[countryValue][0], "");
+
+  if (constraint.test(postalCode.value)) {
+    postalCodeError.textContent = "";
+  } else {
+    postalCodeError.textContent = constraints[countryValue][1];
+  }
+}
+
+country.addEventListener("change", checkPostalCode);
+postalCode.addEventListener("input", checkPostalCode);
